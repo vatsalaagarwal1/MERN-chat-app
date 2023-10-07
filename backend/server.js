20eb2f638d1d470eb886197f243e5cad33e85338
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2023-10-07 16:34:37 +0530

    created backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { chats } = require("./data/data");
 
 const app = express()
 
@@ -6,4 +7,7 @@ app.get('/', (req, res) => {
     res.send("API is running");
 })
 
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
 app.listen(5000, console.log("Server started on PORT 5000"));
