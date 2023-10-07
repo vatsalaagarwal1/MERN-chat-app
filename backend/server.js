b1a79b3096232f94f937609ab15a8e169ccdceac
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2023-10-07 16:59:50 +0530

    created nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const { chats } = require("./data/data");
 
-const app = express()
+const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("API is running");
+    res.send("API is running successfully");
 })
 
 app.get('/api/chat', (req, res) => {
     res.send(chats);
 })
-app.listen(5000, console.log("Server started on PORT 5000"));
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat);
+    // console.log(req.params.id);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`Server started on PORT ${PORT}`));
